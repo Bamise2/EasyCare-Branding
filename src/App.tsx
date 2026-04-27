aa60efe87c810d0d60c8e6875ef15ffc86fec33c
bamise <bamiseshogbesan2004@gmail.com>
2026-04-27 09:19:52 +0100

    Added vercel credentials

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,18 @@ import DesignPortfolio from "./pages/DesignPortfolio"
 import WebsitePortfolio from "./pages/WebsitePortfolio"
 import { Routes, Route } from "react-router-dom"
 
+import { SpeedInsights } from "@vercel/speed-insights/react"
+
 export default function () {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/design-portfolio" element={<DesignPortfolio />} />
-      <Route path="/website-portfolio" element={<WebsitePortfolio />} />
-    </Routes>
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/design-portfolio" element={<DesignPortfolio />} />
+        <Route path="/website-portfolio" element={<WebsitePortfolio />} />
+      </Routes>
+
+      <SpeedInsights />
+    </>
   )
 }
